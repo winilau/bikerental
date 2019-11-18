0394f802a5adf4c3dd3b6b0c8680bdcdd1aadba5
kenza-amira <kenza.amira1.KA@gmail.com>
2019-11-18 20:16:35 +0100

    u know

--- src/uk/ac/ed/bikerental/BikeType.java
+++ src/uk/ac/ed/bikerental/BikeType.java
@@ -4,9 +4,29 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class BikeType {
-	List<BikeType> types = new ArrayList<BikeType>();
-	public void addType(BikeType bikeType) {
-		types.add(bikeType);
+	private enum Type {
+		MOUNTAIN,
+		CYCLOCROSS,
+		TOURING,
+		TANDEM,
+		ELECTRIC,
+		DOWNHILL,
+		BMX,
+		FOLDING,
+		RECUMBENT,
+		CRUISER,
+		HYBRID,
+		ROAD,
+		TRIATHLON,
+		COMMUTING,
+		TRACK,
+		KIDS,	
+	}
+	
+	private final Type typeValue;
+	
+	public BikeType (String type) {
+		typeValue = Type.valueOf(type.toUpperCase());
 	}
     public BigDecimal getReplacementValue() {
         // TODO: Implement Bike.getReplacementValue

fac79f0a075d3cb369f8d1b28d2aae3641780cd0
Wini Lau <winilau@c067011.wlan.net.ed.ac.uk>
2019-11-18 17:40:38 +0000

    test

---
+++ tests/uk/ac/ed/bikerental/MultidayRateTests.java
@@ -0,0 +1,19 @@
+package uk.ac.ed.bikerental;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+
+public class MultidayRateTests {
+
+}

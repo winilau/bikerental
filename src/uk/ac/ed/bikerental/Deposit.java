670d947cb3fc8ff3f4874cde8d898992dbcfaaa5
winilau <winilau@Winis-MacBook-Pro.local>
2019-11-23 22:14:33 +0000

    afd

--- src/uk/ac/ed/bikerental/Deposit.java
+++ src/uk/ac/ed/bikerental/Deposit.java
@@ -1,5 +1,15 @@
 package uk.ac.ed.bikerental;
 
-public class Deposit {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+public class Deposit implements ValuationPolicy {
+	
+
+	@Override
+	public BigDecimal calculateValue(Bike bike, LocalDate date) {
+		BigDecimal result;
+		result =  bike.getReplacementValue();
+		return null;
+	}
 }

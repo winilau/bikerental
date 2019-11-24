21cad9bf7ea3cff91786cb60a601ee837b1acd69
kenza-amira <kenza.amira1.KA@gmail.com>
2019-11-24 01:38:54 +0100

    Dear lord, help us

--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -6,15 +6,37 @@ public class Booking {
 	private int bookingNum;
 	private DateRange duration;
 	private BigDecimal totalPrice;
-	private boolean collection;
+	private boolean pickUp;
 	private Customer customer;
 
-	public Booking(int bookingNum, DateRange duration, BigDecimal totalPrice, boolean collection, Customer customer) {
+	public Booking(int bookingNum, DateRange duration, BigDecimal totalPrice, boolean pickUp, Customer customer) {
 		this.bookingNum = bookingNum;
 		this.duration = duration;
 		this.totalPrice = totalPrice;
-		this.collection = collection;
+		this.pickUp = pickUp;
 		this.customer = customer;
 
 	}
+
+	public int getBookingNum() {
+		return bookingNum;
+	}
+
+	public DateRange getDuration() {
+		return duration;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public boolean isToBePickedUp() {
+		return pickUp;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+	
+	
 }

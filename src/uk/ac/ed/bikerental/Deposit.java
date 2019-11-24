5a814dd29bf6c093a4821b2095cd5cdfe987a4a1
winilau <winilau@Winis-MacBook-Pro.local>
2019-11-24 00:27:17 +0000

    k

--- src/uk/ac/ed/bikerental/Deposit.java
+++ src/uk/ac/ed/bikerental/Deposit.java
@@ -2,6 +2,7 @@ package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Collection;
 
 public class Deposit implements ValuationPolicy {
 	
@@ -12,5 +13,20 @@ public class Deposit implements ValuationPolicy {
 		BigDecimal depositRates = provider.getDepositRate();
 		BigDecimal result =  (bike.getReplacementValue().multiply(depositRates)).stripTrailingZeros();
 		return result;
+	}
+	
+	public BigDecimal calculateValue(Bike bike) {
+		Provider provider = bike.getProvider();
+		BigDecimal depositRates = provider.getDepositRate();
+		BigDecimal result =  (bike.getReplacementValue().multiply(depositRates)).stripTrailingZeros();
+		return result;
+	}
+	
+	public BigDecimal calculateAllValue(Collection<Bike> bikes) {
+		BigDecimal totalDeposit = BigDecimal.ZERO;
+		for (Bike b:bikes) {
+			totalDeposit = totalDeposit.add(calculateValue(b));
+		}
+		return totalDeposit;
 	}
 }

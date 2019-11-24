1dd5ab6a595eb6a0e43bbd345ee296829765a8ac
winilau <winilau@Winis-MacBook-Pro.local>
2019-11-24 01:08:10 +0000

    asd

--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -1,6 +1,8 @@
 package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Booking {
 	private int bookingNum;
@@ -8,14 +10,19 @@ public class Booking {
 	private BigDecimal totalPrice;
 	private boolean pickUp;
 	private Customer customer;
+	private Collection<Bike> bikes = new ArrayList<>();
+	private Provider provider;
 
-	public Booking(int bookingNum, DateRange duration, BigDecimal totalPrice, boolean pickUp, Customer customer) {
+	public Booking(int bookingNum, DateRange duration, BigDecimal totalPrice,
+			boolean pickUp, Customer customer, Collection<Bike> bikes, Provider provider) {
 		this.bookingNum = bookingNum;
 		this.duration = duration;
 		this.totalPrice = totalPrice;
 		this.pickUp = pickUp;
 		this.customer = customer;
-
+		this.bikes = bikes;
+		this.provider = provider;
+		
 	}
 
 	public int getBookingNum() {
@@ -38,5 +45,12 @@ public class Booking {
 		return customer;
 	}
 	
+	public Collection<Bike> getBikes(){
+		return bikes;
+	}
+	
+	public Provider getProvider() {
+		return provider;
+	}
 	
 }

c1afcdb4c240cc380b9688f12dab6abcf51ebb54
winilau <s1846175@muguruza.inf.ed.ac.uk>
2019-11-28 22:38:11 +0000

    yayyyy

---
+++ src/uk/ac/ed/bikerental/SystemClass.java
@@ -0,0 +1,105 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SystemClass {
+	public Map<Integer,Booking> bookingId = new HashMap<>();
+	private int bookingNum = 0;
+	public Collection<Quote> availableQuotes = new ArrayList<>();
+	public Map<Provider, Collection<Bike>> providerBikes = new HashMap<>();
+	public Collection<Provider> providers = new ArrayList<>(); 
+	public MultidayRate calcP = new MultidayRate();  //for calculation
+	public Deposit calcDeposit = new Deposit();    	 //for calculation
+	
+	
+	public Collection<Quote> getQuotes(DateRange dateRange, Map<BikeType, Integer> preference) {
+		for (Provider p: providers) {
+			Collection<Bike> matched = new ArrayList<>();
+			for (Map.Entry<BikeType, Integer> entry: preference.entrySet()) {
+				BikeType type = entry.getKey();
+				Integer amount = entry.getValue();
+				int count = 0;
+				Collection<Bike> bikes = p.getBikes();
+				for (Bike b:bikes) {
+					if (b.getType() == type && b.getAvailability(dateRange)) {
+						if (count < amount) {
+							matched.add(b);
+						}
+						count++;
+					}
+				}
+				if (count < amount) {
+					matched.clear();
+					break;
+				}
+				
+			}
+			if (!matched.isEmpty() ) {
+				BigDecimal totalPrice = calcP.calculatePrice(matched, dateRange).stripTrailingZeros();
+				BigDecimal totalDeposit = calcDeposit.calculateAllValue(matched).stripTrailingZeros();
+				Quote result = new Quote(p, matched, dateRange, totalPrice, totalDeposit);
+				System.out.println(result.getProvider().getName());
+				printBikes(result.getBike());
+				System.out.println(totalPrice);
+				System.out.println(totalDeposit);
+				System.out.println();
+				availableQuotes.add(result);
+			}			
+		}
+		for (Quote q: availableQuotes) {
+			System.out.println(q.toString());
+		}
+		return availableQuotes;
+		
+	}
+	
+	public void printProviders() {
+		for (Provider p:providers) {
+			System.out.println(p.getName());
+		}
+	}
+	
+	public void printBikes(Collection<Bike> bikes) {
+		for (Bike b:bikes) {
+			System.out.println(b.getType().getString() + " ");
+		}
+	}
+	
+	public void addProvider(Provider provider) {
+		providers.add(provider);
+	}
+
+	/**
+	 * 
+	 * @param q          chosen quote by customer
+	 * @param customer   the customer booking
+	 * @param pickUp     if false, customer doesn't pick up bike hence delivery
+	 * 					 if true, customer picks up bike
+	 * @return			 the booking of the chosen quote
+	 */
+	public Booking bookQuote(Quote q, Customer customer, boolean pickUp) {
+		BigDecimal totalPrice = (q.getDeposit().add(q.getPrice())).stripTrailingZeros();  
+	   																					
+		BigDecimal separateDeposit = q.getDeposit(); 
+		DateRange duration = q.getDuration();    
+		Collection <Bike> wantedBikes = q.getBike();   
+		Provider p = q.getProvider(); 
+		Booking booked = new Booking(this.bookingNum, duration, totalPrice, pickUp, customer,wantedBikes, p, separateDeposit);
+		bookingId.put(bookingNum, booked); 
+		this.bookingNum++;            
+		for (Bike b: wantedBikes) {   
+			b.changeAvailability(duration);
+		}
+		//have to implement the delivery service
+		if (!pickUp) {
+			
+		}
+		return booked;
+
+	}
+	
+}

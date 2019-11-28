c20ef2ac4c7db1d4dda9518c1d5433bfd8b22ff8
kenza-amira <kenza.amira1.KA@gmail.com>
2019-11-28 19:07:53 +0100

    You get greggs at 6PM

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -5,43 +5,60 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {	
-    private String pc1,pc2,pc3,pc4;
-    private Location lo1,lo2,lo3,lo4;
+    private String pc1,pc2,pc3,pc4,pc5,pc6,pc7;
+    private Location lo1,lo2,lo3,lo4,lo5,lo6,lo7;
     
     @BeforeEach
     void setUp() throws Exception {
         
         pc1 = "EH81PE";
-        pc2 = "AB13PR";
+        pc2 = "EH13PR";
         pc3 = "EH106QU";
         pc4 = "DA81PE";
+        pc5 = "EH81PE";
+        pc6 = "SR81PE";
+        pc7 = "AT4WAL";
         
-        lo1 = new Location(pc1,"happyland");
-        lo2 = new Location(pc2, "sadkingdom");
-        lo3 = new Location(pc3, "place");
-        lo4 = new Location(pc4, "place");
+        
+        lo1 = new Location(pc1,"Slytherin");
+        lo2 = new Location(pc2, "Griffyndor");
+        lo3 = new Location(pc3, "Hogwarts School of Witchcraft and Wizardry");
+        lo4 = new Location(pc4, "Hufflepuff");
+        lo5 = new Location(pc5, "Ravenclaw");
+        lo6 = new Location(pc6, "Forbidden Forest");
+        lo7 = new Location(pc7, "Diagon Alley");
+        
 
     }
     
     @Test
 	void testIsNear1() {
-    	assertEquals(true, lo1.isNearTo(lo3));
+    	assertEquals(true, lo1.isNearTo(lo2));
 	}
     
     @Test
 	void testIsNear2() {
-    	assertEquals(false, lo2.isNearTo(lo3));
+    	assertEquals(true, lo2.isNearTo(lo1));
 	}
     
     @Test
 	void testIsNear3() {
-    	assertEquals(false, lo4.isNearTo(lo3));
+    	assertEquals(true, lo3.isNearTo(lo5));
 	}
     
     @Test
 	void testIsNear4() {
-    	assertEquals(false, lo1.isNearTo(lo2));
+    	assertEquals(false, lo6.isNearTo(lo4));
 	}
     
+    @Test
+   	void testIsNear5() {
+       	assertEquals(false, lo4.isNearTo(lo6));
+   	}
+    
+	void testIsNear6() {
+       	assertEquals(false, lo7.isNearTo(lo3));
+   	}
+	
     
 }
